aec5f3823bb3658dab652ed9e9bb473f5eab18de
Fredrick Sebastian <futurefamily154@gmail.com>
2025-07-20 15:47:53 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
+   // Select toggle button and nav links
+const shopNow = document.getElementById("shopNow");
+const heroLinks = document.getElementById("heroLinks");
+
+// Add click event to toggle the menu
+shopNow.addEventListener("click", () => {
+  heroLinks.classList.toggle("active");
+});
+
    document.getElementById('productForm').addEventListener('submit', function(e) {
       e.preventDefault();
       document.getElementById('orderConfirmation').style.display = 'block';
